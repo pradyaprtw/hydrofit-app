1812cc501994397d65db7edd85d665b47b8c981c
pradyaprtw <pradiaheningp.com>
2025-11-19 06:07:48 +0700

    Updated database

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,21 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WaterIntakeController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 // 1. Halaman Depan (Redirect ke Login)
 Route::get('/', function () {
     return redirect()->route('login');
+});
+
+// Route Rahasia untuk Migrate dari Vercel
+Route::get('/tembak-database', function () {
+    try {
+        // Jalankan perintah migrate:fresh --force
+        Artisan::call('migrate:fresh --force');
+        return '<h1>BERHASIL! Database sudah di-reset dan diisi. ✅</h1>';
+    } catch (\Exception $e) {
+        return '<h1>GAGAL!</h1><br>' . $e->getMessage();
+    }
 });
 
 // 2. Grup Route yang Wajib Login
@@ -35,16 +46,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // Route Halaman Tentang Aplikasi
 Route::get('/about', [WaterIntakeController::class, 'about'])->name('about');   
 
-// Route Rahasia untuk Migrate dari Vercel
-Route::get('/tembak-database', function () {
-    try {
-        // Jalankan perintah migrate:fresh --force
-        Artisan::call('migrate:fresh --force');
-        return '<h1>BERHASIL! Database sudah di-reset dan diisi. ✅</h1>';
-    } catch (\Exception $e) {
-        return '<h1>GAGAL!</h1><br>' . $e->getMessage();
-    }
-});
 
     // --- PROFILE BAWAAN BREEZE ---
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
